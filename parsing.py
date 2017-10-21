07ccf311cccf3e38e99c1ebc3387e8d23b188283
NIRAJ J DEDHIA <niraj.dedhia@ahold.com>
2017-10-21 15:01:41 -0400

    * Stored .dat to Dictionary

--- parsing.py
+++ parsing.py
@@ -1,34 +1,81 @@
-#OCT - 20
-#movieFormat
+# Program to converts movie data in .DAT
+# file to .csv file.
+# Author : Niraj Dedhia
 # python ./parsing.py
+
+# This is the main function which initiates the program
+# Fetch the data from movies and ratings file and store 
+# them in a dictionary then convert that dictionary in 
+# to .csv file.
 def main():
 
-	readFile();
-	print(movieDictionary["1"]);
-
-# Reading a .DAT file line by line
-# passing this line to parseLine function for storing information
-def readFile():
-
-	f = open("ratings.dat","r")
-	for line in f:
-	    parseLine(line[:-1]);
-
-# This function parse the line meaning convert String value to proper
-# Format. Extracting attributes from Line and then storing them in to 
-# Dictionary.
-def parseLine(line):
-	entryInFile = line.split("::");
-	fieldsDictionary = {};
+	#readFileForParsing("../BIGFILES/movies.dat");
+	#readFileForParsing("../BIGFILES/ratings.dat",1);
+	readFileForParsing("movies.dat");
+	readFileForParsing("ratings.dat",1);
+	calculateAverageRating();
+	print(movieDictionary);
 	
-	splitEntry = entryInFile[1].split("(");
+
+# Reads the .DAT file line by line
+# Based on the option it reads movies and rating files 
+# respectively.
+# Forwards the line to parse it and to fetch the information
+def readFileForParsing(fileName, option=0):
+
+	f = open(fileName,"r");
+	if(option == 0):
+		for movie in f:
+			storeMovieInDictinary(movie[:-1]);
+	else:
+		for rating in f:
+			storeRatingsInDictinary(rating[:-1]);
+	
+
+# Parse the line having movie information and
+# stores the fetched data in to dictionary object.
+def storeMovieInDictinary(movie):
+	fieldsDictionary = {};
+	movieAttributes = movie.split("::");
+	splitEntry = movieAttributes[1].split("(");
+
 	fieldsDictionary['NAME'] = splitEntry[0].strip();
 	fieldsDictionary['YEAR'] = splitEntry[1].strip()[:-1];
-	fieldsDictionary['GENRE'] = entryInFile[2].strip().split("|");
+	fieldsDictionary['GENRE'] = movieAttributes[2].strip().split("|");
+	fieldsDictionary['RATINGS'] = [];
 
-	movieDictionary[entryInFile[0]] = fieldsDictionary;
+	movieDictionary[movieAttributes[0]] = fieldsDictionary;
 
 
-# Gloabl variable which stores Movie information.
+# Parse the line having ratings information and
+# stores them in dictionary object.
+def storeRatingsInDictinary(rating):
+	fileAttributes = rating.split("::");
+	movieDictionary[fileAttributes[1]]['RATINGS'].append(float(fileAttributes[2]));
+
+
+# Thi function calculates the average ratings for
+# every movie
+def calculateAverageRating():
+	for movieID in movieDictionary:
+		movieDictionary[movieID]['RATING'] = float(average(movieDictionary[movieID]['RATINGS']));
+
+
+# Calculates the average for given list
+def average(list):
+	lengthOfList = len(list);
+
+	if(lengthOfList == 0):
+		return 0;
+
+	result = 0;
+	for element in list:
+		result += float(element);
+	result /= lengthOfList;
+
+	return result;
+
+
+# Global variable which stores Movie information.
 movieDictionary = {};
 main();
