5ce0d844d9f992ed4996d1587c820790fc548f86
NIRAJ J DEDHIA <niraj.dedhia@ahold.com>
2017-10-28 00:19:15 -0400

    * Implemented a function which generates Excel file from Dictionary

--- parsing.py
+++ parsing.py
@@ -3,18 +3,21 @@
 # Author : Niraj Dedhia
 # python ./parsing.py
 
+import xlwt;
+
 # This is the main function which initiates the program
 # Fetch the data from movies and ratings file and store 
 # them in a dictionary then convert that dictionary in 
 # to .csv file.
 def main():
 
-	#readFileForParsing("../BIGFILES/movies.dat");
-	#readFileForParsing("../BIGFILES/ratings.dat",1);
+	# readFileForParsing("../BIGFILES/movies.dat");
+	# readFileForParsing("../BIGFILES/ratings.dat",1);
 	readFileForParsing("movies.dat");
 	readFileForParsing("ratings.dat",1);
 	calculateAverageRating();
-	print(movieDictionary);
+	covertToExcel();
+	#print(movieDictionary);
 	
 
 # Reads the .DAT file line by line
@@ -41,7 +44,7 @@ def storeMovieInDictinary(movie):
 
 	fieldsDictionary['NAME'] = splitEntry[0].strip();
 	fieldsDictionary['YEAR'] = splitEntry[1].strip()[:-1];
-	fieldsDictionary['GENRE'] = movieAttributes[2].strip().split("|");
+	fieldsDictionary['GENRE'] = movieAttributes[2].strip();
 	fieldsDictionary['RATINGS'] = [];
 
 	movieDictionary[movieAttributes[0]] = fieldsDictionary;
@@ -54,11 +57,34 @@ def storeRatingsInDictinary(rating):
 	movieDictionary[fileAttributes[1]]['RATINGS'].append(float(fileAttributes[2]));
 
 
-# Thi function calculates the average ratings for
+# This function calculates the average ratings for
 # every movie
 def calculateAverageRating():
 	for movieID in movieDictionary:
 		movieDictionary[movieID]['RATING'] = float(average(movieDictionary[movieID]['RATINGS']));
+
+
+# This function converts dictionary to excel file
+def covertToExcel():
+	excelFile = xlwt.Workbook(encoding="utf-8");
+	sheet = excelFile.add_sheet("Sheet");
+
+	sheet.write(0, 0, "Movie ID");
+	sheet.write(0, 1, "Movie Name");
+	sheet.write(0, 2, "Movie Rating");
+	sheet.write(0, 3, "Movie Genere");
+
+	row = 1;
+
+	for movieID in movieDictionary:
+		sheet.write(row, 0, movieID);
+		sheet.write(row, 1, movieDictionary[movieID]['NAME']);
+		sheet.write(row, 2, movieDictionary[movieID]['RATING']);
+		sheet.write(row, 3, movieDictionary[movieID]['GENRE']);
+		row += 1;
+
+	excelFile.save("Movie_Dataset_File");
+		
 
 
 # Calculates the average for given list
